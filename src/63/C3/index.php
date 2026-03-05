4701a366e182464376b6cb36ea23f30a5d3d4a6b
kosei <koush0811@gmail.com>
2026-03-05 14:11:51 +0900

    テーブル生成

---
+++ src/63/C3/index.php
@@ -0,0 +1,57 @@
+<?php 
+    $values = [
+        [
+            "id" => 1,
+            "name" => "John Doe",
+            "email" => "john@example.com"
+        ],
+        [
+            "id" => 2,
+            "name" => "Jane Smith",
+            "email" => "jane@example.com"
+        ],
+        [
+            "id" => 3,
+            "name" => "Peter Jones",
+            "email" => "peter@example.com"
+        ]
+    ]
+?>
+
+<style>
+    table{
+        border-collapse: collapse;
+    }
+    th,td{
+        border: solid 1px black;
+        box-sizing: border-box;
+        padding: 10px;
+    }
+
+</style>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <table>
+        <tr>
+            <th>ID</th>
+            <th>Name</th>
+            <th>Email</th>
+        </tr>
+
+        <?php foreach($values as $value): ?>
+            <tr>
+                <td><?= $value["id"] ?></td>
+                <td><?= $value["name"] ?></td>
+                <td><?= $value["email"] ?></td>
+            </tr>
+        <?php  endforeach;?>             
+    </table>
+</body>
+</html>
