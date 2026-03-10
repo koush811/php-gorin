740c13387c32a2fa139a2b0b3431c3725f1a87e5
kosei <koush0811@gmail.com>
2026-03-10 22:09:33 +0900

    csv

--- src/62/D3/index.php
+++ src/62/D3/index.php
@@ -1,4 +1,24 @@
 <?php 
+    $file = "d3.json";
+    $json = file_get_contents($file);
+    $data = json_decode($json, true);
+
+    if(isset($_POST["export"])){
+        header("Content-Type: text/csv;");
+        header("Content-Disposition: attachment; filename=d3.csv");
+        //CSVファイルとしてダウンロードさせる
+        $fp = fopen("php://output", "w");
+
+        fputcsv($fp, array_keys($data[0]));
+        //fputcsv:配列をCSV形式で書き込む関数
+        foreach($data as $dev){
+            fputcsv($fp,$dev);
+        }
+
+        fclose($fp);
+        //CSV書き込み終了
+        exit;
+    }
 
 ?>
 
@@ -10,6 +30,28 @@
     <title>Document</title>
 </head>
 <body>
-    
+    <h1>CSV</h1>
+    <table>
+        <tr>
+            <?php 
+                foreach(array_keys(($data[0])) as $key){
+                    echo "<th>$key</th>";
+                }
+            ?>
+        </tr>
+        
+        <?php 
+            foreach($data as $dev){
+                echo "<tr>";
+                foreach($dev as $value){
+                    echo "<td>$value</td>";
+                }
+                echo "<tr>";
+            }
+        ?>
+    </table>
+    <form method="post">
+        <button type="submit" name="export">エクスポート</button>
+    </form>
 </body>
 </html>
