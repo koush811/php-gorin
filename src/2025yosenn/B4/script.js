cc124974e3281ae198614768aba82603c4ac1a3e
kosei <koush0811@gmail.com>
2026-03-21 22:19:08 +0900

    予選問題B4

--- src/2025yosenn/B4/script.js
+++ src/2025yosenn/B4/script.js
@@ -1,8 +1,14 @@
 const body = document.querySelector("body")
 
 body.addEventListener("mousemove",(e)=>{
-    x = e.clientX
-    y = e.clientY
+    const clientX = e.clientX
+    const clientY = e.clientY
 
-    body.style.backgroundColor = `rgb(${x},${y},256)`
+    const width = window.innerWidth
+    const height = window.innerHeight
+
+    const x = Math.floor((clientX / width)*255)
+    const y = Math.floor((clientY / height)*255)
+
+    body.style.backgroundColor = `rgb(${x},${y},255)`
 })
