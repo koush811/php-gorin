3d21e6129ac29bcef463c53285aa30fac41e2bc7
kosei <koush0811@gmail.com>
2026-03-31 11:03:45 +0900

    61回 C1

---
+++ src/61/C1/script.js
@@ -0,0 +1,31 @@
+const text = document.getElementById('text')
+const button = document.querySelector('button')
+const input = document.querySelector("input")
+
+function Color(){
+    return`
+        rgb(
+            ${Math.floor(Math.random()*255)},
+            ${Math.floor(Math.random()*255)},
+            ${Math.floor(Math.random()*255)}
+        )
+    `
+}
+
+button.addEventListener("click",()=>{
+    const word = input.value
+    console.log(word)
+
+    let original = text.textContent
+
+    if(word === "")return
+
+    const regex = new RegExp(word,"g")
+
+    const hilight = original.replace(regex,function(match){
+        const color = Color();
+        return `<span style="background-color:${color};">${match}</span>`
+    })
+
+    text.innerHTML = hilight;
+})
